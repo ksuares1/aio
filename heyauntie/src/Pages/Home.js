ebad2c9730e0a497ed2440126db89f7dad49960b
ksuares1 <kimberlyjsuares@gmail.com>
2022-07-15 22:59:25 -0400

    added image to main and css to image

---
+++ heyauntie/src/Pages/Home.js
@@ -0,0 +1,17 @@
+import MainPage from "../components/Main";
+
+
+
+
+let Home =() =>{
+    return(
+        <main>
+            <div class="heyauntie-img">
+                <MainPage/>
+
+            </div>
+        </main>
+    )
+}
+
+export default Home;
---
+++ heyauntie/src/components/Main.js
@@ -0,0 +1,16 @@
+
+
+
+
+let MainPage=() =>{
+    return (
+        <main>
+            <div class="homepage">
+                <img class="main-background" src={require("../assets/img/main-img.PNG")} alt="heyauntie-main-img" />
+            
+            </div>
+        </main>
+    )
+}
+
+export default MainPage;
