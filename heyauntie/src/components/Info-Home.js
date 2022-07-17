b6a62e0b9b65b3a583c1fce9e1de6cc8702b6b73
ksuares1 <kimberlyjsuares@gmail.com>
2022-07-17 01:01:31 -0400

    added height to card

--- heyauntie/src/components/Info-Home.js
+++ heyauntie/src/components/Info-Home.js
@@ -1,10 +1,10 @@
 import Button from 'react-bootstrap/Button';
-
+import Card from 'react-bootstrap/Card';
 
 let InfoQuestions =() =>{
     return(
         <>
-        <div class="main-info">
+        {/* <div class="main-info">
         <h1>A community to help you rise and thrive.</h1>
         <p>Hey Auntie is a culturally sensitive, safe, supportive, and the unique challenges we face in our everyday lives.</p>
         </div>
@@ -15,7 +15,31 @@ let InfoQuestions =() =>{
         <Button variant="secondary" size="sm">
           Request an Invite
         </Button>
+      </div> */}
+     
+
+    <Card style={{ width: '18rem', height:'35%'}}>
+      <Card.Body>
+        <Card.Title>A community to help you rise and thrive.</Card.Title>
+        <br/>
+        <Card.Subtitle className="mb-2 text-muted">Hey Auntie is a culturally sensitive, safe, supportive, and the unique challenges we face in our everyday lives</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <div>
+        <Button variant="primary" size="sm">
+          Login
+        </Button>{' '}
+        <Button variant="secondary" size="sm">
+          Request an Invite
+        </Button>
       </div>
+        {/* <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link> */}
+      </Card.Body>
+    </Card>
+  
       
       </>
     )
