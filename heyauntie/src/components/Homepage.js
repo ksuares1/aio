7e46cb889c475631d7530a09331d9dd2a6e16230
ksuares1 <kimberlyjsuares@gmail.com>
2022-07-19 14:32:12 -0400

    added text to homepage container

--- heyauntie/src/components/Homepage.js
+++ heyauntie/src/components/Homepage.js
@@ -7,7 +7,8 @@ const Homepage=()=>{
                 <img src={require("../assets/img/heyauntie-ladies.png")}  alt="two-woman-dicussing-heyauntie" />
             </div>
             <div class="text-container">
-                    <h1> Some Text</h1>
+                    <h1> A community to help you rise and thrive.</h1> 
+                    <p> HeyAuntie is a culturally sensitive, safe, supportive,and accessible, platform for black women, and the unique challenges we face in our everyday lives.</p>
                 </div>
             
         </div>
