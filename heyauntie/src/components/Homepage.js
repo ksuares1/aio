a9cda0aa19eea8e10ef1da4d1ba35d34de09de17
ksuares1 <kimberlyjsuares@gmail.com>
2022-07-19 14:52:00 -0400

    updated buttons and links

--- heyauntie/src/components/Homepage.js
+++ heyauntie/src/components/Homepage.js
@@ -1,4 +1,4 @@
-
+import Button from 'react-bootstrap/Button';
 
 const Homepage=()=>{
     return(
@@ -8,7 +8,23 @@ const Homepage=()=>{
             </div>
             <div class="text-container">
                     <h1> A community to help you rise and thrive.</h1> 
+                    <br/>
                     <p> HeyAuntie is a culturally sensitive, safe, supportive,and accessible, platform for black women, and the unique challenges we face in our everyday lives.</p>
+                    <br/>
+                    <div class="btns-container">
+                    <div className="mb-2">
+                        <a href="/login">
+                <Button id="log-btn" variant="primary" size="lg">
+                     LOGIN
+                     </Button>{' '}
+                     </a>
+                     <a href="/questions">
+                <Button id="invite" variant="secondary" size="lg">
+                    Request an Invite
+                </Button>
+                </a>
+      </div>
+                    </div>
                 </div>
             
         </div>
