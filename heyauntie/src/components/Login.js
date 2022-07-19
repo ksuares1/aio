0d9dee66224c340ddec7dcb01d6efaa8cea10032
ksuares1 <kimberlyjsuares@gmail.com>
2022-07-19 01:41:02 -0400

    added link to profile img

--- heyauntie/src/components/Login.js
+++ heyauntie/src/components/Login.js
@@ -6,13 +6,16 @@ const Signup =() =>{
         
         <div class="profile-img">
         <div class="blue-img">
+            <a href="/home">
             <img href="/" class="blue-logo"  src={require('../assets/img/blue-logo.png')} alt="heyauntie-blue-logo" />
+            </a>
             </div>
             <br/>
             <div class="container">
             <div class="profile-btns" col-md-12 text-center>  
            <Button variant="outline-primary">Login with Facebook</Button>{' '}
            </div>
+           <br/>
            <div>
       <Button variant="outline-secondary">Login with Gmail</Button>{' '}
             </div>
