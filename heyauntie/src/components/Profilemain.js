b55c18675f01625ce7d12e41b59a7864c04c979b
ksuares1 <kimberlyjsuares@gmail.com>
2022-07-22 01:19:38 -0400

    added links for auntee and auntie pages

--- heyauntie/src/components/Profilemain.js
+++ heyauntie/src/components/Profilemain.js
@@ -10,10 +10,12 @@ const MainProfile=() =>{
         <div class="heyladies">
             <div class="user-idenitifies">
             <h1 >Ask a question</h1>
+                <a href="auntie">
                 <h3 >I'm an auntie</h3>
-             
+                </a>
+                <a href="auntee">
                 <h3>I'm an auntee</h3>
-
+                </a>
         <img src={require("../assets/img/heyauntie-ladies.png")} alt="heyauntie-ladies"/>
         </div>
         </div>
